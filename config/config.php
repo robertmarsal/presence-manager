085fc977e8b37c92e86a052cd8a3952cc07cb0db
Robert Boloc <robertboloc@gmail.com>
2012-04-11 22:18:47 +0200

    production config

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $CONFIG = new stdClass();
-
+/*
 $CONFIG->wwwroot = 'https://localhost/presence-manager';
 $CONFIG->apiroot = $CONFIG->wwwroot.'/api/json';
 
@@ -12,5 +12,16 @@ $CONFIG->dbhost = 'localhost';
 $CONFIG->dbname = 'presence';
 $CONFIG->dbuser = 'root';
 $CONFIG->dbpassword = 'doremi';
+*/
+$CONFIG->wwwroot = 'https://presence-manager.orchestra.io';
+$CONFIG->apiroot = $CONFIG->wwwroot.'/api/json';
+
+$CONFIG->debug= 1;
+$CONFIG->verbose = 1;
+
+$CONFIG->dbhost = 'a.db.shared.orchestra.io';
+$CONFIG->dbname = 'db_fbaebcba';
+$CONFIG->dbuser = 'user_fbaebcba';
+$CONFIG->dbpassword = 'ampuTM8vpEULhq';
 
 date_default_timezone_set('UTC');
