85f3db9cb23fd09b8217eea0fcc00934e89f8df5
Robert Boloc <robertboloc@gmail.com>
2012-07-02 21:07:59 +0200

    fixed some css on the menu

--- app/helpers/MenuHelper.php
+++ app/helpers/MenuHelper.php
@@ -111,7 +111,7 @@ class MenuHelper extends Helper {
 
         return
             '<ul class="nav pull-right no-hover-a">
-                <p class="navbar-text pull-right"><a href="' . $CONFIG->wwwroot . '/auth/logout"><b>' . $STRINGS['logout'] . '</b></a></p>
+                <p class="logout pull-right"><a href="' . $CONFIG->wwwroot . '/auth/logout"><b>' . $STRINGS['logout'] . '</b></a></p>
              </ul>';
     }
 }
