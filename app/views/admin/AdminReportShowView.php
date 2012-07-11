15e38e4131cc57395a4c362a1f2b2b185df22893
Robert Boloc <robertboloc@gmail.com>
2012-07-11 18:08:16 +0200

    updated report view

--- app/views/admin/AdminReportShowView.php
+++ app/views/admin/AdminReportShowView.php
@@ -24,8 +24,12 @@ class AdminReportShowView extends View{
         foreach($this->_data->intervals as $interval){
             $intervals_list .='<tr>
                 <td>'.$interval->h.'h  '.$interval->i.'m  '.$interval->s.'s</td>
-                <td>'.date('G:i:s D M j Y', $interval->timestart).'</td>
-                <td>'.date('G:i:s D M j Y', $interval->timestop).'</td>
+                <td><span class="label label-success">
+				'.date('G:i:s D M j Y', $interval->timestart).'
+				</span></td>
+                <td><span class="label label-important">
+				'.date('G:i:s D M j Y', $interval->timestop).'
+				</span></td>
                 </tr>';
         }
         $intervals_list.= '</table>';
