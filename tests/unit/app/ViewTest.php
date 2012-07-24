e339f706a5b8c154e7119752437b92f3083294cb
Robert Boloc <robert.boloc@urv.cat>
2012-07-24 11:28:30 +0200

    removed old test

--- tests/unit/app/ViewTest.php
+++ tests/unit/app/ViewTest.php
@@ -6,32 +6,17 @@ class ViewClassTest extends PHPUnit_Framework_TestCase {
 
     /**
      * Mock of the abstract class 'View'
-     * 
+     *
      * @var View
      */
     protected $view;
 
     /**
-     * Creates a mock of the abstract class 'View'  
+     * Creates a mock of the abstract class 'View'
      */
     protected function setUp() {
         $this->view = $this->getMockForAbstractClass('View');
     }
-    
-    /**
-     * Tests if the title is set correctly
-     * 
-     * @group unit
-     */
-    public function testSetTitle(){
-        //get & call the title method with the 'Foo Bar' value
-        $title_method = TestHelper::getAccessibleMethod('View', 'title');
-        $title_method->invokeArgs($this->view, array('Foo Bar'));
-        //get the title attribute
-        $title_attr = TestHelper::getAccessibleAttribute('View', '_title');
-        //check if the title is set
-        $this->assertEquals('Foo Bar', $title_attr->getValue($this->view));
-    }
-   
+
     protected function tearDown() {}
 }
