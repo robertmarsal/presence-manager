2151a8ba5785ca0ecaa757727a301fa33fc914ab
Robert Boloc <robertboloc@gmail.com>
2012-08-18 20:41:40 +0200

    added catalan translation

--- lang/ca.php
+++ lang/ca.php
@@ -1,1 +1,85 @@
 <?php
+
+$STRINGS = null;
+
+//page titles, menu
+$STRINGS['brand'] = 'Presence';
+$STRINGS['login'] = 'Accedeix';
+$STRINGS['help'] = 'Ajuda';
+$STRINGS['activity'] = 'Activitat';
+$STRINGS['users'] = 'Usuaris';
+$STRINGS['report'] = 'Informe';
+$STRINGS['user'] = 'Usuari';
+$STRINGS['logout'] = 'Surt';
+$STRINGS['home'] = 'Inici';
+$STRINGS['profile'] = 'Perfil';
+$STRINGS['404'] = '404';
+$STRINGS['details'] = 'Detalls';
+$STRINGS['summary'] = 'Resum';
+$STRINGS['account'] = 'Compte';
+$STRINGS['starttime'] = 'Temps Inici';
+$STRINGS['startdate'] = 'Data Inici';
+$STRINGS['endtime'] = 'Temps Fi';
+$STRINGS['enddate'] = 'Data Fi';
+$STRINGS['action'] = 'Acció';
+$STRINGS['time'] = 'Temps';
+$STRINGS['date'] = 'Data';
+$STRINGS['firstname'] = 'Nom';
+$STRINGS['lastname'] = 'Cognoms';
+$STRINGS['issued'] = 'Emès';
+$STRINGS['period'] = 'Periode';
+$STRINGS['intervals'] = 'Intervals';
+$STRINGS['interval'] = 'Interval';
+$STRINGS['incidences'] = 'Incidencies';
+$STRINGS['total'] = 'Total';
+$STRINGS['password'] = 'Contrasenya';
+$STRINGS['position'] = 'Posició';
+$STRINGS['role'] = 'Rol';
+$STRINGS['update'] = 'Actualitza';
+$STRINGS['week'] = 'Setmana';
+$STRINGS['month'] = 'Mes';
+$STRINGS['year'] = 'Any';
+$STRINGS['type'] = 'Tipus';
+$STRINGS['message'] = 'Missatge';
+$STRINGS['previous'] = 'Anterior';
+$STRINGS['next'] = 'Següent';
+$STRINGS['day'] = 'Dia';
+$STRINGS['identifier'] = 'Identificador';
+$STRINGS['uuid'] = 'UUID';
+$STRINGS['mac'] = 'MAC';
+$STRINGS['useraccountnotice'] = 'Per raons de seguretat l\'UUID i el MAC només es poden consultar i no actualitzar';
+$STRINGS['period'] = 'Periode';
+
+//user
+$STRINGS['user:update:success'] = 'Els detalls del usuari s\'han actualitzat amb éxit!';
+$STRINGS['user:update:failed'] = 'La actualització dels detalls ha fallats!';
+$STRINGS['user:delete:success'] = 'Usuari eliminat correctament!';
+$STRINGS['user:delete:failed'] = 'La eliminació del usuari ha fallat!';
+$STRINGS['user:create:success'] = 'S\'ha creat el nou usuari!';
+$STRINGS['user:create:failed'] = 'Error al crear un nou usuari!';
+$STRINGS['user:create:duplicate'] = 'Aquest usuari ja extisteix!';
+
+//activity
+$STRINGS['admin:no:activities'] = 'No hi ha mes activitat!';
+
+//users
+$STRINGS['add:user'] = 'Afegir usuari';
+$STRINGS['create:user'] = 'Crear Usuari';
+
+//report
+$STRINGS['build:report'] = 'Genera Informe';
+
+//events
+$STRINGS['event:success'] = 'Èxit!';
+$STRINGS['event:error'] = 'Error!';
+$STRINGS['event:checkin'] = 'Check-In';
+$STRINGS['event:checkout'] = 'Check-Out';
+$STRINGS['event:incidence'] = 'Incidencia';
+$STRINGS['event:noactivity'] = 'Sense Activitat!';
+$STRINGS['event:noactivity:message'] = 'No hi ha activitat';
+$STRINGS['event:noactivityinterval:message'] = 'No hi ha activitat per aquest usuari en l\'interval seleccionat';
+$STRINGS['event:noactivitythisweek:message'] = 'No hi ha activitat per aquesta setmana';
+$STRINGS['event:nousers'] = 'Sense usuaris';
+$STRINGS['event:nouser:message'] = 'No queden usuaris per mostrar';
+$STRINGS['event:delete:account'] = 'Elimina compte';
+$STRINGS['event:delete:account:warning'] = 'Compte! Aquesta acció no es pot desfer!';
